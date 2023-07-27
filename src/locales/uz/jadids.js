f4ff6202bdfe744b92cd1eac8f3f8362c7a497a1
bekpulatov <94681980+shovotlandiya@users.noreply.github.com>
2023-07-27 09:01:47 +0500

    changed text

--- src/locales/uz/jadids.js
+++ src/locales/uz/jadids.js
@@ -2,9 +2,9 @@ export const jadids = {
   qodiriy: "Abdulla Qodiriy",
   shakuriy: "Abduqodir Shakuriy",
   abdurashidhonov: "Abdurashidhonov, Munavvar qori",
-  avloniy: "Avloniy,Abdulla",
-  behbudiy: "Behbudi,Mahmud Xo'ja",
-  fitrat: "Fitrat,Abdurauf",
-  ayniy: "Ayniy,Sadriddin",
+  avloniy: "Avloniy Abdulla",
+  behbudiy: "Behbudi Mahmud Xo'ja",
+  fitrat: "Fitrat Abdurauf",
+  ayniy: "Ayniy Sadriddin",
   shukrullayev: "Xoji Muin Shukrullayev",
 };
